87052cc7b1acc8c0703c343cd4a8a69ec56748fe
Jiten Rai <raijiten96@gmail.com>
2025-02-17 14:07:12 +0545

    minor updates

--- voice_assistant.py
+++ voice_assistant.py
@@ -30,10 +30,12 @@ def speechtx(x):
     engine.runAndWait()
 
 if __name__ == '__main__':
-    speechtx("Hello I am Jarvis.")
+    print('Hello I am Jarvis AI your voice-commanded virtual assistant.')
+    speechtx("Hello I am Jarvis AI your voice-commanded virtual assistant.")
     while True:
         command = sptext().lower()
         if 'jarvis' in command: 
+            print('I am ready for your command.')
             speechtx("I am ready for your command.")
             while True:
                     data1 = sptext().lower()
@@ -44,15 +46,19 @@ if __name__ == '__main__':
                             webbrowser.open(site[1])
                     if "how are you" in data1:
                         reply = 'I am doing great, How are you?'
+                        print(reply)
                         speechtx(reply)
                     if 'your name' in data1:
-                        name = "My name is Javis."
+                        name = "It's so silly for you to ask again. My name is Javis."
+                        print(name)
                         speechtx(name)
                     elif 'old are you' in data1:
-                        age = "I have just been created!"
+                        age = "I have been created quite a while ago. By Jiten Rai, of course!"
+                        print(age)
                         speechtx(age)
                     elif "time now" in data1:
                         time = datetime.datetime.now().strftime("%I%M%p") #%I gives hour, %M gives minute, %p gives am pm
+                        print(f"The time is {time}")
                         speechtx(f"The time is {time}")
                     # elif 'youtube' in data1:
                     #     webbrowser.open('https://www.youtube.com/')
@@ -62,15 +68,16 @@ if __name__ == '__main__':
                         joke = pyjokes.get_joke(language='en', category='all')
                         print(joke)
                         speechtx(joke)
-                    elif 'music' in data1:
-                        add = r'C:\Users\ACER\OneDrive\Desktop\New folder'
-                        list_of_song = os.listdir(add)
-                        print(list_of_song)
-                        os.startfile(os.path.join(add, list_of_song[0]))
+                    # elif 'music' in data1:
+                    #     add = r'C:\Users\ACER\OneDrive\Desktop\New folder'
+                    #     list_of_song = os.listdir(add)
+                    #     print(list_of_song)
+                    #     os.startfile(os.path.join(add, list_of_song[0]))
                     elif 'open chrome' in data1:
                         speechtx('Opening google chrome.')
                         os.startfile(r'C:\Users\Public\Desktop\Google Chrome.lnk')
                     elif "exit" in data1:
+                        print('I am always at your service. Come again.')
                         speechtx("I am always at your service. Come again.")
                         break    
                     # time.sleep(5)
